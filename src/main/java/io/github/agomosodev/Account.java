6b68cd605b67bd7fca3c6d022203654f17269e95
antonio-admin <antoniojgo1499@gmail.com>
2025-11-19 18:06:20 +0100

    UPD: Attributes added to account completed

--- src/main/java/io/github/agomosodev/Account.java
+++ src/main/java/io/github/agomosodev/Account.java
@@ -1,6 +1,15 @@
 package io.github.agomosodev;
 
+import java.time.LocalDate;
+
 public class Account {
+    private String accountNumber;
+    private double balance;
+    private Client owner;
+    private String accountType;
+    private boolean state;
+    private LocalDate creationDate;
+
     public Account() {
     }
 }
